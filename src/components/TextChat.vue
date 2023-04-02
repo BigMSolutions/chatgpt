d17b11aaf39801e1d3b058a26990271cd1761933
BigMSolutions <mossaab.rafih@gmail.com>
2023-04-02 12:08:58 +0000

    init

---
+++ src/components/TextChat.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="q-pa-md row justify-center">
+    <div style="width: 100%; max-width: 400px">
+      <q-chat-message
+        v-for="m in messages"
+        :key="m.index"
+        :name="m.sender"
+        :text="[m.message]"
+        :sent="m.sender != 'chatgpt'"
+      />
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: ["messages"],
+};
+</script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,34 @@
+<template>
+  <q-page class="row">
+    <chat :messages="messages" />
+  </q-page>
+  <q-footer elevated class="bg-grey-8 text-white">
+    <div class="row">
+      <q-input outlined v-model="text" label="Outlined" />
+      <q-btn color="primary" icon="mail" label="On Left" />
+    </div>
+  </q-footer>
+</template>
+
+<script>
+import chat from "../components/TextChat.vue";
+export default {
+  components: {
+    chat,
+  },
+  data() {
+    return {
+      messages: [
+        {
+          sender: "me",
+          message: "test 1",
+        },
+        {
+          sender: "chatgpt",
+          message: "respense one",
+        },
+      ],
+    };
+  },
+};
+</script>
